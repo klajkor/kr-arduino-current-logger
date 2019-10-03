b974ebfca0f8c7cb759e2f0e6effb7a7827585d0
Robert Klajko <klajkor@gmail.com>
2019-10-03 21:42:23 +0200

    Refactoring, variable reorg - 20191003

--- Arduino_current_meter_logger.ino
+++ Arduino_current_meter_logger.ino
@@ -7,24 +7,25 @@
 **  - SD card, SPI
 **********************************************/
 
-/*
+/* Arduino Nano pinout connections
 
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
+ ** I2C bus:
+ ** SCK - pin A5
+ ** SDA - pin A4
 
- */
+ ** SD card on SPI bus:
+ ** MOSI - pin D11
+ ** MISO - pin D12
+ ** CLK - pin D13
+ ** CS - pin D4
+*/
 
-#include <SPI.h>
+#include <Arduino.h>
+#include <uRTCLib.h>
 #include <SD.h>
-
-#include "Arduino.h"
-#include "uRTCLib.h"
+#include <SPI.h>
 #include <Wire.h>
 #include <INA219.h>
-#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -34,13 +35,14 @@
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
+const int OLED_I2C_ADDR = 0x3C; // Address 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Current and voltage sensor
 INA219 monitor;
 
 // DS3231 RTC modul
-#define RTC_I2C_addr 0x68
+const int RTC_I2C_addr = 0x68;
 uRTCLib rtc(RTC_I2C_addr);
 
 // SD card modul
@@ -69,34 +71,40 @@ String CurrentString = "";
 void setup()
 {
   Serial.begin(115200);
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  //display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR)) {
+    Serial.println(F("SSD1306 allocation failed - HALTED"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  Serial.println(F("SSD1306 init ok"));
+  Serial.print(F("SD card init..."));
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SDCARD_CHIP_SELECT)) {
+    Serial.println(F("Card failed, or not present -> HALTED"));
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println(F("done."));
+
   Wire.begin();
+  Serial.println(F("Wire.begin done"));
   monitor.begin();
+  Serial.println(F("monitor.begin done"));
   // begin calls:
   // configure() with default values RANGE_32V, GAIN_8_320MV, ADC_12BIT, ADC_12BIT, CONT_SH_BUS
   // calibrate() with default values D_SHUNT=0.1, D_V_BUS_MAX=32, D_V_SHUNT_MAX=0.2, D_I_MAX_EXPECTED=2
   // in order to work directly with ADAFruit's INA219B breakout
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(SDCARD_CHIP_SELECT)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
-  Serial.println("card initialized.");
   display.clearDisplay();
   display.display();
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
-  display.print(F("SD card init done"));
   display.display();
   delay(200);
   display.clearDisplay();
@@ -113,78 +121,96 @@ void loop()
   rtc_day = rtc.day();
   rtc_month = rtc.month();
   rtc_year = rtc.year();
-
+  
+  TimeStampString = F("20");
+  TimeStampString += String(rtc_year);
+  TimeStampString += F(".");
+  //TimeStampString = TimeStampString + String(rtc_year) + F(".");
+  //display.print(F("20"));
+  //display.print(rtc_year);
+  //display.print(F("."));
+  if(rtc_month<10) {
+    //display.print(F("0"));
+    TimeStampString += F("0");
+  }
+  TimeStampString += String(rtc_month);
+  //display.print(rtc_month);
+  TimeStampString += F(".");
+  //display.print(F("."));
+  if(rtc_day<10) {
+    //display.print(F("0"));
+    TimeStampString += F("0");
+  }
+  //display.print(rtc_day);
+  TimeStampString += String(rtc_day);
+  //display.print(F(" "));
+  TimeStampString += F(" ");
+  if(rtc_hour<10) {
+    //display.print(F("0"));
+    TimeStampString += F("0");
+  }
+  //display.print(rtc_hour);
+  TimeStampString += String(rtc_hour);
+  //display.print(F(":"));
+  TimeStampString += F(":");
+  if(rtc_minute<10) {
+    //display.print(F("0"));
+    TimeStampString += F("0");
+  }
+  //display.print(rtc_minute);
+  TimeStampString += String(rtc_minute);
+  //display.print(F(":"));
+  TimeStampString += F(":");
+  if(rtc_second<10) {
+    //display.print(F("0"));
+    TimeStampString += F("0");
+  }
+  //display.print(rtc_second);
+  TimeStampString += String(rtc_second);
+  
+  // clear display
   display.clearDisplay();
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
+  display.println(TimeStampString);
 
-  Serial.println("******************");
+  Serial.println(TimeStampString);
 
-  Serial.print("raw shunt voltage: ");
+  Serial.print(F("raw shunt voltage: "));
   Serial.println(monitor.shuntVoltageRaw());
 
-  Serial.print("raw bus voltage:   ");
+  Serial.print(F("raw bus voltage:   "));
   Serial.println(monitor.busVoltageRaw());
 
-  Serial.println("--");
+  Serial.println(F("--"));
 
-  Serial.print("shunt voltage: ");
+  Serial.print(F("shunt voltage: "));
   Serial.print(monitor.shuntVoltage() * 1000, 4);
-  Serial.println(" mV");
+  Serial.println(F(" mV"));
 
-  Serial.print("shunt current: ");
+  Serial.print(F("shunt current: "));
   Serial.print(monitor.shuntCurrent() * 1000, 4);
-  Serial.println(" mA");
+  Serial.println(F(" mA"));
 
-  TimeStampString = F("20");
-  TimeStampString += String(rtc_year);
-  display.print(F("20"));
-  display.print(rtc_year);
-  display.print(F("."));
-  if(rtc_month<10) {
-    display.print(F("0"));
-  }
-  display.print(rtc_month);
-  display.print(F("."));
-  if(rtc_day<10) {
-    display.print(F("0"));
-  }
-  display.print(rtc_day);
-  display.print(F(" "));
-  if(rtc_hour<10) {
-    display.print(F("0"));
-  }
-  display.print(rtc_hour);
-  display.print(F(":"));
-  if(rtc_minute<10) {
-    display.print(F("0"));
-  }
-  display.print(rtc_minute);
-  display.print(F(":"));
-  if(rtc_second<10) {
-    display.print(F("0"));
-  }
-  display.print(rtc_second);
-  display.println(F(" "));
   //display.print(F("Current: "));
   display.print(monitor.shuntCurrent()*1000, 4);
   display.println(F(" mA"));
 
-  Serial.print("bus voltage:   ");
+  Serial.print(F("bus voltage:   "));
   Serial.print(monitor.busVoltage(), 4);
-  Serial.println(" V");
+  Serial.println(F(" V"));
 
   //display.print(F("Bus V: "));
   display.print(monitor.busVoltage(), 4);
   display.println(F(" V"));
 
-  Serial.print("bus power:     ");
+  Serial.print(F("bus power:     "));
   Serial.print(monitor.busPower() * 1000, 4);
-  Serial.println(" mW");
+  Serial.println(F(" mW"));
 
-  Serial.println(" ");
-  Serial.println(" ");
+  Serial.println(F(" "));
+  Serial.println(F(" "));
 
   display.display();
 
